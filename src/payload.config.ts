77f067e50592513c5d61a524e208468930799c4e
Aleck Shen <aleckshn@gmail.com>
2026-03-20 15:50:25 +1300

    feat(.github): set up ci pipeline workflow (#2)
    * feat(.github): added ci pipeline
    
    * fix(payload): change connection string name to correct one
    
    * chore(lint): lint everything

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,15 +1,14 @@
-import { mongooseAdapter } from "@payloadcms/db-mongodb";
-import { lexicalEditor } from "@payloadcms/richtext-lexical";
-import path from "path";
-import { buildConfig } from "payload";
-import { fileURLToPath } from "url";
-import sharp from "sharp";
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import { mongooseAdapter } from "@payloadcms/db-mongodb"
+import { lexicalEditor } from "@payloadcms/richtext-lexical"
+import { buildConfig } from "payload"
+import sharp from "sharp"
+import { Media } from "./payload/collections/Media"
+import { Users } from "./payload/collections/Users"
 
-import { Users } from "./payload/collections/Users";
-import { Media } from "./payload/collections/Media";
-
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 export default buildConfig({
   admin: {
@@ -25,8 +24,8 @@ export default buildConfig({
     outputFile: path.resolve(dirname, "payload-types.ts"),
   },
   db: mongooseAdapter({
-    url: process.env.DATABASE_URL || "",
+    url: process.env.DATABASE_URI || "",
   }),
   sharp,
   plugins: [],
-});
+})
