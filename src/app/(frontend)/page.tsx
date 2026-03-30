c87f29d236e778229be6d80852c9f51aea1442e7
aleckshen <aleckshn@gmail.com>
2026-03-30 21:22:26 +1300

    feat(tailwind): import tailwind css

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import { getPayload } from "payload"
 
 import config from "@/payload.config"
-import "./styles.css"
+import "./globals.css"
 
 export default async function HomePage() {
   const headers = await getHeaders()
