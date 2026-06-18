0dc1654be3c7a56bdae094b42e8f9329edc2e1c8
Ashlee Shum <ashlee@shum.co.nz>
2026-06-18 16:22:38 +1200

    Revert "feat(components): created faq dropdown accordian  (#67)"
    This reverts commit 288b3573b046c62c415faca1f39f60a190d72a7a.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,4 @@
-
-
 export { EventCard } from "./EventCard/EventCard"
 export { Footer } from "./Footer/Footer"
 export { Logo } from "./Logo/Logo"
-export { Faq } from "./FAQ/FAQ"
-
 export { Navbar } from "./Navbar/Navbar"
