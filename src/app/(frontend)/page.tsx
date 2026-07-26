9f3fb1006c2401e2f5e652b77f089ff7cc88a69c
Shimpei Yumiba <shimpeiyumiba@gmail.com>
2026-07-26 15:52:20 +1200

    fix(components): change faq in page.tsx to be self-closing

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -40,6 +40,6 @@ export default function FaqPage() {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
         },
       ]}
-    ></Faq>
+    />
   )
 }
