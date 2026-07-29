4703d5b18c5bf675d18fc97fdf5553c8411fa599
Jason <jasonlimnzz@gmail.com>
2026-07-29 14:04:11 +1200

    fix(components): align UpcomingEvents to match more closely with figma design

--- src/components/UpcomingEvents/UpcomingEvents.tsx
+++ src/components/UpcomingEvents/UpcomingEvents.tsx
@@ -17,35 +17,35 @@ const events = [
 
 export default function UpcomingEvents() {
   return (
-    <section className="bg-brand-primary py-20">
+    <section className="bg-[#0E2BB3] py-20">
       <div className="mx-auto max-w-7xl px-6">
-        <h2 className="text-center font-extrabold text-6xl text-white uppercase md:text-8xl">
+        <h1 className="text-center font-heading text-7xl text-white uppercase md:text-9xl">
           Upcoming Events
-        </h2>
+        </h1>
 
-        <p className="mx-auto mt-6 text-center text-white">
+        <p className="mx-auto mt-8 max-w-5xl text-center text-base text-white md:text-xl mb-30">
           The University of Auckland Volleyball Club caters to players of all skill levels, even
           those outside UOA!
         </p>
 
-        <div className="mt-20 grid gap-12 md:grid-cols-2">
+        <div className="mt-20 flex flex-wrap justify-center gap-20">
           {events.map((event) => (
-            <div className="flex flex-col items-center" key={event.title}>
-              <div className="w-full max-w-125 overflow-hidden transition-transform duration-300 hover:scale-110">
+            <div className="flex w-full max-w-lg flex-col items-center" key={event.title}>
+              <div className="w-full overflow-hidden rounded-lg shadow-lg transition-transform duration-300 hover:scale-110">
                 <Image
                   alt={event.title}
-                  className="h-80 w-full object-cover"
-                  height={320}
+                  className="h-96 w-full object-cover"
+                  height={384}
                   src={event.image}
-                  width={500}
+                  width={576}
                 />
               </div>
 
-              <h3 className="mt-8 text-center font-bold text-4xl text-white uppercase">
+              <h3 className="mt-8 text-center font-heading text-4xl text-white uppercase md:text-5xl">
                 {event.title}
               </h3>
 
-              <div className="mt-8">
+              <div className="mt-10">
                 <Button
                   asChild
                   className="border-2 border-brand-yellow"
