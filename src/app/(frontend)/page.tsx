5724209a4abb7c9c44a160b4be847c5f083a7fdc
Youdaox <youdao005@gmail.com>
2026-08-03 17:46:10 +1200

    restore homepage after testing

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,3 @@
-import { AwardsBanner } from "@/components"
-
-const AWARDS = ["2025 Sports Club of the Year (Runner-up)", "2021 Supreme Club of the Year"]
-
 export default function HomePage() {
-  return (
-    <div>
-      <AwardsBanner awards={AWARDS} />
-    </div>
-  )
+  return <div />
 }
