909cd95361e476058244fd69969c7e6b8e7bcc3a
Youdaox <youdao005@gmail.com>
2026-09-18 17:56:51 +1200

    feat: show create account form on sign-up page

--- src/app/(frontend)/sign-up/page.tsx
+++ src/app/(frontend)/sign-up/page.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next"
 
+import { CreateAccountForm } from "@/components"
+
 export const metadata: Metadata = {
-  title: "Sign Up",
+  title: "Create Account",
 }
 
 export default function SignUpPage() {
   return (
-    <main className="flex flex-col items-center px-6 py-20">
+    <main className="flex flex-col items-center gap-12 px-6 pt-12 pb-32">
       <h1 className="text-center font-heading text-6xl text-brand-primary uppercase sm:text-8xl">
-        Sign Up
+        Create Account
       </h1>
+      <CreateAccountForm />
     </main>
   )
 }
